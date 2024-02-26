b85413842f157be63f37c47d1da56075bd9c1ad0
Merin Cherian <38310878+merincherian@users.noreply.github.com>
2024-02-26 14:34:21 +0000

    Update 3childextrac.py

--- DDICorpus/Train/DrugBank/3childextrac.py
+++ DDICorpus/Train/DrugBank/3childextrac.py
@@ -1,5 +1,8 @@
 #Extracting sentences with one or three child elements
 
+# ONLY CONSIDERING, sentences with relationships among two entities.
+# getchildren was deprecated
+
 import xml.etree.ElementTree as ET
 import re
 import random
@@ -8,20 +11,20 @@ from os import path
 with open("file.txt") as f1:
     text1 = f1.read()
 
-
 filenames = re.split(r'[\n\r]+', text1)
+filenames_trial = ["Abarelix_ddi.xml"]
 
 for filename in filenames:
 	print (filename)	
 	tree = ET.parse(filename)
 	root = tree.getroot()
 
-
 	for sentence in root.findall('sentence'):
 		#rank = sentence.find('pair')
-		if (len(sentence.getchildren())!=3):
+		# print(list(sentence))
+		if (len(list(sentence))!=3):
 			root.remove(sentence)
 
-	newfilepath = path.join('Edited',filename)
+	newfilepath = path.join('Edited_TEST',filename)
 	print (newfilepath)
 	tree.write(newfilepath)
